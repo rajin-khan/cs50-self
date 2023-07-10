9276b72f5016753fdbf7e7b4b501dd2236d36fc1
Rajin Khan <rajin.khan2001@gmail.com>
2023-07-10 19:09:52 +0600

    Update compare

--- C/compare.c
+++ C/compare.c
@@ -3,12 +3,22 @@
 int main(void) {
 
     int x;
+    int y;
 
     printf("What's x? ");
-    scanf("%d", &x)
+    scanf("%d", &x);
 
     printf("What's y? ");
-    scanf("%d", &x)
+    scanf("%d", &y);
+
+    if (x < y) {
+
+        printf("x is less than y\n");
+    }
+    else {
+
+        printf("x is not less than y\n");
+    }
     
     return 0;
 }
